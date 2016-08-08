754c65908863ee63842607714bba3802b81387fa
George Marques <george@gmarqu.es>
2016-08-07 21:44:27 -0300

    Very basic version of packaging working!
    - Depends on a Content_Types file available.
    - No compression.

--- platform/winrt/export/export.cpp
+++ platform/winrt/export/export.cpp
@@ -41,6 +41,498 @@
 #include "io/base64.h"
 #include "bind/core_bind.h"
 
+class AppxPackager {
+
+	enum {
+		FILE_HEADER_MAGIC = 0x04034b50,
+		DATA_DESCRIPTOR_MAGIC = 0x08074b50,
+		CENTRAL_DIR_MAGIC = 0x02014b50,
+		END_OF_CENTRAL_DIR_MAGIC = 0x06054b50,
+		ZIP64_END_OF_CENTRAL_DIR_MAGIC = 0x06064b50,
+		ZIP64_END_DIR_LOCATOR_MAGIC = 0x07064b50,
+		ZIP64_HEADER_ID = 0x0001,
+		ZIP_VERSION = 0x2d,
+		GENERAL_PURPOSE = 0x08,
+		BASE_FILE_HEADER_SIZE = 30,
+		DATA_DESCRIPTOR_SIZE = 24,
+		BASE_CENTRAL_DIR_SIZE = 46,
+		EXTRA_FIELD_LENGTH = 0x1c,
+		ZIP64_HEADER_SIZE = 0x18,
+		ZIP64_END_OF_CENTRAL_DIR_SIZE = 44,
+		END_OF_CENTRAL_DIR_SIZE = 42,
+	};
+
+	struct BlockHash {
+
+		String base64_hash;
+		size_t compressed_size;
+	};
+
+	struct FileMeta {
+
+		String name;
+		int lfh_size;
+		size_t compressed_size;
+		size_t uncompressed_size;
+		Vector<BlockHash> hashes;
+		uLong file_crc32;
+		ZPOS64_T zip_offset;
+	};
+
+	EditorProgress *progress;
+	FileAccess *package;
+	String tmp_blockmap_file_name;
+
+	Set<String> mime_types;
+
+	Vector<FileMeta> file_metadata;
+
+	ZPOS64_T central_dir_offset;
+	ZPOS64_T end_of_central_dir_offset;
+	size_t central_dir_size;
+
+	String hash_block(uint8_t* p_block_data, size_t p_block_len);
+
+	void make_block_map();
+
+	void make_file_header(uint8_t *p_buf, String p_name, bool p_compress);
+	void make_file_descriptor(uint8_t* p_buf, uint32_t p_crc32, size_t p_compressed_size, size_t p_uncompressed_size);
+	void make_central_dir_header(uint8_t* p_buf, const FileMeta p_file);
+	void write_zip64_end_of_central_record();
+	void write_end_of_central_record();
+
+public:
+
+	void init(FileAccess* p_fa, EditorProgress* p_progress);
+	void add_file(String p_file_name, const uint8_t* p_buffer, size_t p_len, bool p_compress = false, bool p_make_hash = true);
+	void finish();
+};
+
+String AppxPackager::hash_block(uint8_t * p_block_data, size_t p_block_len) {
+
+	char hash[32];
+	char base64[45];
+
+	sha256_context ctx;
+	sha256_init(&ctx);
+	sha256_hash(&ctx, p_block_data, p_block_len);
+	sha256_done(&ctx, (uint8_t*)hash);
+
+	base64_encode(base64, hash, 32);
+	base64[44] = '\0';
+
+	return String(base64);
+}
+
+void AppxPackager::make_block_map() {
+
+	FileAccess* tmp_file = FileAccess::open(tmp_blockmap_file_name, FileAccess::WRITE);
+
+	tmp_file->store_string("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+	tmp_file->store_string("<BlockMap xmlns=\"http://schemas.microsoft.com/appx/2010/blockmap\" HashMethod=\"http://www.w3.org/2001/04/xmlenc#sha256\">");
+
+	for (int i = 0; i < file_metadata.size(); i++) {
+
+		FileMeta file = file_metadata[i];
+
+		tmp_file->store_string(
+			"<File Name=\"" + file.name.replace("/","\\")
+			+ "\" Size=\"" + itos(file.uncompressed_size)
+			+ "\" LfhSize=\"" + itos(file.lfh_size) + "\">");
+
+
+		// <Block Hash="HKjnnQ/pWYOjvpXHoeXz0LPUHuFH7DL5jujS6+f+bsI=" Size="853"/>
+		for (int j = 0; j < file.hashes.size(); j++) {
+
+			tmp_file->store_string("<Block Hash=\""
+				+ file.hashes[j].base64_hash + "\" Size=\""
+				+ itos(file.hashes[j].compressed_size) + "\"/>");
+		}
+
+		tmp_file->store_string("</File>");
+	}
+
+	tmp_file->store_string("</BlockMap>");
+
+	tmp_file->close();
+	tmp_file = NULL;
+}
+
+void AppxPackager::make_file_header(uint8_t *p_buf, String p_name, bool p_compress) {
+
+	int name_len = p_name.length();
+
+	// Write magic
+	p_buf[0] = 0x50;
+	p_buf[1] = 0x4b;
+	p_buf[2] = 0x03;
+	p_buf[3] = 0x04;
+
+	// Version
+	p_buf[4] = 0x2d;
+	p_buf[5] = 0x00;
+
+	// Special flag
+	p_buf[6] = 0x08;
+	p_buf[7] = 0x00;
+
+	// Compression
+	p_buf[8] = p_compress ? 0x08 : 0x00;
+	p_buf[9] = 0x00;
+
+	// Empty header data
+	for (int i = 10; i < 26; i++) {
+		p_buf[i] = 0x00;
+	}
+
+	// File name length (little-endian)
+	p_buf[26] = name_len & 0xFF;
+	p_buf[27] = (name_len >> 8) & 0xFF;
+
+	// Extra data length
+	p_buf[28] = 0x00;
+	p_buf[29] = 0x00;
+
+	// File name
+	for (int i = 0; i < name_len; i++) {
+		p_buf[i + 30] = p_name.utf8().get(i);
+	}
+
+	// Done!
+}
+
+void AppxPackager::make_file_descriptor(uint8_t* p_buf, uint32_t p_crc32, size_t p_compressed_size, size_t p_uncompressed_size) {
+
+	// Write magic
+	for (int i = 0; i < 4; i++) {
+		p_buf[i] = (DATA_DESCRIPTOR_MAGIC >> (i*8)) & 0xFF;
+	}
+
+	// CRC
+	for (int i = 0; i < 4; i++) {
+		p_buf[i + 4] = (p_crc32 >> (i*8)) & 0xFF;
+	}
+
+	// Compressed size
+	for (int i = 0; i < 8; i++) {
+		p_buf[i + 8] = (p_compressed_size >> (i*8)) & 0xFF;
+	}
+
+	// Uncompressed size
+	for (int i = 0; i < 8; i++) {
+		p_buf[i + 16] = (p_uncompressed_size >> (i*8)) & 0xFF;
+	}
+
+	// Done!
+}
+
+void AppxPackager::make_central_dir_header(uint8_t* p_buf, const FileMeta p_file) {
+
+	int offs = 0;
+
+	// Write magic
+	for (int i = 0; i < 4; i++) {
+		p_buf[offs++] = (CENTRAL_DIR_MAGIC >> (i*8)) & 0xFF;
+	}
+	// Version (twice)
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (ZIP_VERSION >> (i*8)) & 0xFF;
+	}
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (ZIP_VERSION >> (i*8)) & 0xFF;
+	}
+	// General purpose flag
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (GENERAL_PURPOSE >> (i * 8)) & 0xFF;
+	}
+
+	// Compression, mod date/time
+	for (int i = 0; i < 6; i++) {
+		p_buf[offs++] = 0;
+	}
+
+	// Crc-32
+	for (int i = 0; i < 4; i++) {
+		p_buf[offs++] = (p_file.file_crc32 >> (i*8)) & 0xFF;
+	}
+
+	// File sizes (will be in extra field)
+	for (int i = 0; i < 8; i++) {
+		p_buf[offs++] = 0xFF;
+	}
+
+	// File name length
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (p_file.name.length() >> (i*8)) & 0xFF;
+	}
+
+	// Extra field length
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (EXTRA_FIELD_LENGTH >> (i*8)) & 0xFF;
+	}
+
+	// Comment length
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = 0;
+	}
+
+	// Disk number start, internal/external file attributes
+	for (int i = 0; i < 8; i++) {
+		p_buf[offs++] = 0;
+	}
+
+	// Relative offset (will be on extra field)
+	for (int i = 0; i < 4; i++) {
+		p_buf[offs++] = 0xFF;
+	}
+
+	// File name
+	for (int i = 0; i < p_file.name.length(); i++) {
+		p_buf[offs++] = p_file.name.utf8().get(i);
+	}
+
+	// Zip64 extra field
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (ZIP64_HEADER_ID >> (i*8)) & 0xFF;
+	}
+	for (int i = 0; i < 2; i++) {
+		p_buf[offs++] = (ZIP64_HEADER_SIZE >> (i*8)) & 0xFF;
+	}
+
+	// Original size
+	for (int i = 0; i < 8; i++) {
+		p_buf[offs++] = (p_file.uncompressed_size >> (i*8)) & 0xFF;
+	}
+	// Compressed size
+	for (int i = 0; i < 8; i++) {
+		p_buf[offs++] = (p_file.compressed_size >> (i*8)) & 0xFF;
+	}
+	// File offset
+	for (int i = 0; i < 8; i++) {
+		p_buf[offs++] = (p_file.zip_offset >> (i*8)) & 0xFF;
+	}
+
+	// Done!
+}
+
+void AppxPackager::write_zip64_end_of_central_record() {
+
+	Vector<uint8_t> buf;
+	buf.resize(ZIP64_END_OF_CENTRAL_DIR_SIZE + 12); // Size plus magic
+
+	int offs = 0;
+
+	// Write magic
+	for (int i = 0; i < 4; i++) {
+		buf[offs++] = (ZIP64_END_OF_CENTRAL_DIR_MAGIC >> (i*8)) & 0xFF;
+	}
+
+	// Size of this record
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (((uint64_t)ZIP64_END_OF_CENTRAL_DIR_SIZE) >> (i*8)) & 0xFF;
+	}
+
+	// Version (yes, twice)
+	for (int i = 0; i < 2; i++) {
+		buf[offs++] = (ZIP_VERSION >> (i*8)) & 0xFF;
+	}
+	for (int i = 0; i < 2; i++) {
+		buf[offs++] = (ZIP_VERSION >> (i*8)) & 0xFF;
+	}
+
+	// Disk number
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = 0;
+	}
+
+	// Number of entries (total and per disk)
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (((uint64_t)file_metadata.size()) >> (i*8)) & 0xFF;
+	}
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (((uint64_t)file_metadata.size()) >> (i*8)) & 0xFF;
+	}
+
+	// Size of central dir
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (((uint64_t)central_dir_size) >> (i*8)) & 0xFF;
+	}
+
+	// Central dir offset
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (central_dir_offset >> (i*8)) & 0xFF;
+	}
+
+	// Done!
+	package->store_buffer(buf.ptr(), buf.size());
+}
+
+void AppxPackager::write_end_of_central_record() {
+
+	Vector<uint8_t> buf;
+	buf.resize(END_OF_CENTRAL_DIR_SIZE);
+
+	int offs = 0;
+
+	// Write magic for zip64 centra dir locator
+	for (int i = 0; i < 4; i++) {
+		buf[offs++] = (ZIP64_END_DIR_LOCATOR_MAGIC >> (i*8)) & 0xFF;
+	}
+
+	// Disk number
+	for (int i = 0; i < 4; i++) {
+		buf[offs++] = 0;
+	}
+
+	// Relative offset
+	for (int i = 0; i < 8; i++) {
+		buf[offs++] = (end_of_central_dir_offset >> (i*8)) & 0xFF;
+	}
+
+	// Number of disks
+	for (int i = 0; i < 4; i++) {
+		buf[offs++] = (1 >> (i*8)) & 0xFF;
+	}
+
+	// Write magic for end central dir
+	for (int i = 0; i < 4; i++) {
+		buf[offs++] = (END_OF_CENTRAL_DIR_MAGIC >> (i*8)) & 0xFF;
+	}
+
+	// Dummy stuff for Zip64
+	for (int i = 0; i < 16; i++) {
+		buf[offs++] = 0xFF;
+	}
+	// Size of comments
+	for (int i = 0; i < 2; i++) {
+		buf[offs++] = 0;
+	}
+
+	// Done!
+	package->store_buffer(buf.ptr(), buf.size());
+}
+
+void AppxPackager::init(FileAccess * p_fa, EditorProgress* p_progress) {
+
+	progress = p_progress;
+	package = p_fa;
+	central_dir_offset = 0;
+	end_of_central_dir_offset = 0;
+	tmp_blockmap_file_name = EditorSettings::get_singleton()->get_settings_path() + "/tmp/tmpblockmap.xml";
+}
+
+void AppxPackager::add_file(String p_file_name, const uint8_t * p_buffer, size_t p_len, bool p_compress, bool p_make_hash) {
+
+	FileMeta meta;
+	meta.name = p_file_name;
+	meta.uncompressed_size = p_len;
+	meta.compressed_size = p_len;
+
+
+	// Create file header
+	Vector<uint8_t> file_header;
+	file_header.resize(BASE_FILE_HEADER_SIZE + p_file_name.length());
+	make_file_header(file_header.ptr(), p_file_name, p_compress);
+	meta.lfh_size = file_header.size();
+	meta.zip_offset = package->get_pos();
+
+	package->store_buffer(file_header.ptr(), file_header.size());
+
+	// Store file
+	package->store_buffer(p_buffer, p_len);
+
+	// Calculate file CRC-32
+	uLong crc = crc32(0L, Z_NULL, 0);
+	crc = crc32(crc, p_buffer, p_len);
+	meta.file_crc32 = crc;
+
+	// Create data descriptor
+	Vector<uint8_t> file_descriptor;
+	file_descriptor.resize(DATA_DESCRIPTOR_SIZE);
+	make_file_descriptor(file_descriptor.ptr(), crc, p_len, p_len);
+
+	package->store_buffer(file_descriptor.ptr(), file_descriptor.size());
+
+	// Make block hashes
+	if (p_make_hash) {
+		Vector<uint8_t> block;
+		block.resize(65536);
+		size_t step = 0;
+
+		while (p_len - step > 0) {
+
+			size_t block_size = (p_len - step) > 65536 ? 65536 : (p_len - step);
+
+			for (int i = 0; i < block_size; i++) {
+				block[i] = p_buffer[step + i];
+			}
+
+			BlockHash bh;
+			bh.base64_hash = hash_block(block.ptr(), block_size);
+			bh.compressed_size = block_size;
+
+			meta.hashes.push_back(bh);
+
+			step += block_size;
+		}
+	}
+
+	file_metadata.push_back(meta);
+}
+
+void AppxPackager::finish() {
+
+	// Create and add block map file
+	make_block_map();
+	FileAccess* blockmap_file = FileAccess::open(tmp_blockmap_file_name, FileAccess::READ);
+	Vector<uint8_t> blockmap_buffer;
+	blockmap_buffer.resize(blockmap_file->get_len());
+
+	blockmap_file->get_buffer(blockmap_buffer.ptr(), blockmap_buffer.size());
+
+	add_file("AppxBlockMap.xml", blockmap_buffer.ptr(), blockmap_buffer.size(), false, false);
+
+	blockmap_file->close();
+	blockmap_file = NULL;
+
+
+	// Add content types
+	FileAccess* types_file = FileAccess::open(EditorSettings::get_singleton()->get_settings_path() + "/tmp/[Content_Types].xml", FileAccess::READ);
+	Vector<uint8_t> types_buffer;
+	types_buffer.resize(types_file->get_len());
+
+	types_file->get_buffer(types_buffer.ptr(), types_buffer.size());
+
+	add_file("[Content_Types].xml", types_buffer.ptr(), types_buffer.size(), false, false);
+
+	types_file->close();
+	types_file = NULL;
+
+
+	// Central directory
+	central_dir_offset = package->get_pos();
+	central_dir_size = 0;
+	for (int i = 0; i < file_metadata.size(); i++) {
+
+		FileMeta file = file_metadata[i];
+		Vector<uint8_t> buff;
+		buff.resize(BASE_CENTRAL_DIR_SIZE + file.name.length() + EXTRA_FIELD_LENGTH);
+
+		central_dir_size += buff.size();
+
+		make_central_dir_header(buff.ptr(), file);
+
+		package->store_buffer(buff.ptr(), buff.size());
+	}
+
+	// End record
+	end_of_central_dir_offset = package->get_pos();
+	write_zip64_end_of_central_record();
+	write_end_of_central_record();
+
+	package->close();
+}
+
 class EditorExportPlatformWinrt : public EditorExportPlatform {
 
 	OBJ_TYPE(EditorExportPlatformWinrt, EditorExportPlatform);
@@ -87,32 +579,10 @@ public:
 
 Error EditorExportPlatformWinrt::save_appx_file(void * p_userdata, const String & p_path, const Vector<uint8_t>& p_data, int p_file, int p_total) {
 
-	AppxExportData *ed = (AppxExportData*)p_userdata;
-	String dst_path = p_path;
-	dst_path = dst_path.replace_first("res://", "");
+	AppxPackager *packager = (AppxPackager*)p_userdata;
+	String dst_path = p_path.replace_first("res://", "");
 
-	int err = zipOpenNewFileInZip(
-		ed->appx,
-		dst_path.utf8().get_data(),
-		NULL,
-		NULL,
-		0,
-		NULL,
-		0,
-		NULL,
-		Z_NO_COMPRESSION,
-		Z_DEFAULT_COMPRESSION
-	);
-
-	if (err != ZIP_OK) return ERR_CANT_CREATE;
-
-	err = zipWriteInFileInZip(ed->appx, p_data.ptr(), p_data.size());
-	zipCloseFileInZip(ed->appx);
-	if (err != ZIP_OK) {
-		return ERR_FILE_CANT_WRITE;
-	}
-
-	ed->ep->step("File: " + p_path, 3 + p_file * 100 / p_total);
+	packager->add_file(dst_path, p_data.ptr(), p_data.size());
 
 	return OK;
 }
@@ -213,6 +683,13 @@ Error EditorExportPlatformWinrt::export_project(const String & p_path, bool p_de
 		}
 	}
 
+	Error err = OK;
+	FileAccess *fa_pack = FileAccess::open(p_path, FileAccess::WRITE, &err);
+	ERR_FAIL_COND_V(err != OK, ERR_CANT_CREATE);
+
+	AppxPackager packager;
+	packager.init(fa_pack, &ep);
+
 	FileAccess *src_f = NULL;
 	zlib_filefunc_def io = zipio_create_io_from_file(&src_f);
 
@@ -228,12 +705,6 @@ Error EditorExportPlatformWinrt::export_project(const String & p_path, bool p_de
 
 	int ret = unzGoToFirstFile(pkg);
 
-	zlib_filefunc_def io2 = io;
-	FileAccess *dst_f = NULL;
-	io2.opaque = &dst_f;
-	String unhashed_path = p_path;//EditorSettings::get_singleton()->get_settings_path() + "/tmp/tmpexport_appx_unhashed.appx";
-	zipFile unhashed_appx = zipOpen2(unhashed_path.utf8().get_data(), APPEND_STATUS_CREATE, NULL, &io2);
-
 	while (ret == UNZ_OK) {
 
 		// get file name
@@ -242,6 +713,11 @@ Error EditorExportPlatformWinrt::export_project(const String & p_path, bool p_de
 		ret = unzGetCurrentFileInfo(pkg, &info, fname, 16834, NULL, 0, NULL, 0);
 
 		String file = fname;
+
+		if (file.ends_with("/")) {
+			ret = unzGoToNextFile(pkg);
+			continue;
+		}
 
 		Vector<uint8_t> data;
 		data.resize(info.uncompressed_size);
@@ -253,168 +729,21 @@ Error EditorExportPlatformWinrt::export_project(const String & p_path, bool p_de
 
 		print_line("ADDING: " + file);
 
-
-		// write
-		const bool uncompressed = info.compression_method == 0;
-
-		zipOpenNewFileInZip(unhashed_appx,
-			file.utf8().get_data(),
-			NULL,
-			NULL,
-			0,
-			NULL,
-			0,
-			NULL,
-			Z_NO_COMPRESSION,
-			Z_DEFAULT_COMPRESSION
-		);
-
-		zipWriteInFileInZip(unhashed_appx, data.ptr(), data.size());
-		zipCloseFileInZip(unhashed_appx);
+		packager.add_file(file, data.ptr(), data.size(), false);
 
 		ret = unzGoToNextFile(pkg);
-
 	}
 
 	ep.step("Adding Files..", 1);
-	Error err = OK;
 	Vector<String> cl;
 
 	gen_export_flags(cl, p_flags);
 
-	AppxExportData ed;
-	ed.appx = unhashed_appx;
-	ed.ep = &ep;
-
-	err = export_project_files(save_appx_file, &ed, false);
+	err = export_project_files(save_appx_file, &packager, false);
 
 	unzClose(pkg);
-	zipClose(unhashed_appx, NULL);
 
-	FileAccess* hashing_f = NULL;
-	zlib_filefunc_def io_hash = io;
-	io_hash.opaque = &hashing_f;
-	unzFile hashing_appx = unzOpen2(unhashed_path.utf8().get_data(), &io_hash);
-	int base_header_size = 30;
-
-	String block_map_path = EditorSettings::get_singleton()->get_settings_path() + "/tmp/tmpexport_appx_blockmap.xml";
-	FileAccess* block_map = FileAccess::open(unhashed_path + "_blockmap.xml", FileAccess::WRITE_READ, &err);
-
-	block_map->store_string("<?xml version=\"1.0\" encoding=\"UTF - 8\" standalone=\"no\"?>\n");
-	block_map->store_string("<BlockMap xmlns=\"http://schemas.microsoft.com/appx/2010/blockmap\" HashMethod=\"http://www.w3.org/2001/04/xmlenc#sha256\">\n");
-
-
-	ep.step("Creating hashes", 103);
-
-	ret = unzGoToFirstFile(hashing_appx);
-
-	while (ret == UNZ_OK) {
-
-		// Get file info
-		// get file name
-		unz_file_info info;
-		char fname[16834];
-		ret = unzGetCurrentFileInfo(hashing_appx, &info, fname, 16834, NULL, 0, NULL, 0);
-
-		int header_size = base_header_size + info.size_filename + info.size_file_extra;
-
-		String name = fname;
-
-		if (name.ends_with("/")) {
-			// Directory, skip
-			ret = unzGoToNextFile(hashing_appx);
-			continue;
-		}
-
-		name = name.replace("/", "\\");
-
-		block_map->store_string("<File Name=\"" + name
-			+ "\" Size=\"" + itos(info.uncompressed_size)
-			+ "\" LfhSize=\"" + itos(header_size) + "\">\n"
-		);
-
-		Vector<uint8_t> block;
-		// 64KB blocks
-		block.resize(65536);
-
-		unzOpenCurrentFile(hashing_appx);
-
-		ZPOS64_T stream_start_pos = unzGetCurrentFileZStreamPos64(hashing_appx);
-		int bytes_read = unzReadCurrentFile(hashing_appx, block.ptr(), 65536);
-		while (bytes_read > 0) {
-
-			uLong offs = unzGetOffset(hashing_appx);
-			ZPOS64_T stream_pos = unzGetCurrentFileZStreamPos64(hashing_appx);
-			z_off_t tell = unztell(hashing_appx);
-
-			int compressed_size = stream_pos - stream_start_pos;
-			stream_start_pos = stream_pos;
-
-
-			print_line("for " + name + " read " + itos(bytes_read) + " bytes.");
-			print_line("    compr: " + itos(compressed_size) + "; offs: " + itos(offs) + "; stream_pos: " + itos(stream_pos) + "; tell: " + itos(tell) + ";");
-
-			sha256_context sha256;
-			sha256_init(&sha256);
-			sha256_hash(&sha256, block.ptr(), bytes_read);
-
-			char hash[32];
-			sha256_done(&sha256, (uint8_t*)hash);
-
-			char base64[60];
-			base64_encode(base64, hash, 32);
-			base64[59] = '\0';
-
-			String hash_string(base64);
-
-			/*block_map->store_string("\t<Block Hash=\""
-				+ hash_string + "\" Size=\""
-				+ itos(compressed_size) + "\"/>\n"
-			);*/
-			block_map->store_string("\t<Block Hash=\""
-				+ hash_string + "\"/>\n"
-			);
-
-			bytes_read = unzReadCurrentFile(hashing_appx, block.ptr(), 65536);
-		}
-
-		block_map->store_string("</File>\n");
-
-		unzCloseCurrentFile(hashing_appx);
-
-		ret = unzGoToNextFile(hashing_appx);
-	}
-
-	unzClose(hashing_appx);
-
-	block_map->seek(0);
-	Vector<uint8_t> blockmap_buffer;
-	blockmap_buffer.resize(block_map->get_len());
-	block_map->get_buffer(blockmap_buffer.ptr(), blockmap_buffer.size());
-	block_map->close();
-
-	/*zlib_filefunc_def io_hashed = io;
-	FileAccess *hashed_f = NULL;
-	io_hashed.opaque = &hashed_f;
-	zipFile hashed_appx = zipOpen2(unhashed_path.utf8().get_data(), APPEND_STATUS_CREATEAFTER, NULL, &io_hashed);
-
-	zipOpenNewFileInZip(
-		hashed_appx,
-		"AppxBlockMap.xml",
-		NULL,
-		NULL,
-		0,
-		NULL,
-		0,
-		NULL,
-		Z_DEFLATED,
-		Z_DEFAULT_COMPRESSION
-	);
-
-	zipWriteInFileInZip(hashed_appx, blockmap_buffer.ptr(), blockmap_buffer.size());
-	zipCloseFileInZip(hashed_appx);
-
-	zipClose(hashed_appx, NULL);*/
+	packager.finish();
 
 	return OK;
 }
