c22445f37ac0bba61860c93ef859af663168cadd
George Marques <george@gmarqu.es>
2019-09-24 12:51:28 -0300

    GDScript: Add parser method to only parse the interface
    Also split the parsing of initializers and function bodies, because
    the interface parse needs one but not the other.

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -5174,7 +5174,7 @@ void GDScriptParser::_parse_class(ClassNode *p_class) {
 	}
 }
 
-void GDScriptParser::_parse_class_contents(ClassNode *p_class) {
+void GDScriptParser::_parse_class_member_initalizers(ClassNode *p_class) {
 	current_class = p_class;
 	// Parse constants
 	tokenizer->reset();
@@ -5194,6 +5194,20 @@ void GDScriptParser::_parse_class_contents(ClassNode *p_class) {
 		p_class->variables.set(i, member);
 	}
 
+	// Should be safe to assume the indent level increased in inner classes
+	IndentLevel current_indent = indent_level.back()->get();
+	current_indent.indent += 1;
+	indent_level.push_back(current_indent);
+	for (int i = 0; i < p_class->subclasses.size(); i++) {
+		ClassNode *subclass = p_class->subclasses[i];
+		_parse_class_member_initalizers(subclass);
+	}
+	indent_level.pop_back();
+	current_class = NULL;
+}
+
+void GDScriptParser::_parse_class_function_bodies(ClassNode *p_class) {
+	current_class = p_class;
 	// Parse each function
 	tokenizer->reset();
 	for (int i = 0; i < p_class->functions.size(); i++) {
@@ -5235,7 +5249,7 @@ void GDScriptParser::_parse_class_contents(ClassNode *p_class) {
 	indent_level.push_back(current_indent);
 	for (int i = 0; i < p_class->subclasses.size(); i++) {
 		ClassNode *subclass = p_class->subclasses[i];
-		_parse_class_contents(subclass);
+		_parse_class_function_bodies(subclass);
 	}
 	indent_level.pop_back();
 	current_class = NULL;
@@ -8641,10 +8655,10 @@ Error GDScriptParser::_parse(const String &p_base_path) {
 
 	if (for_completion) check_types = false;
 
-	// Parse the content of function blocks
+	// Parse the initializers for constants and class variables (for type inference)
 	indent_level.clear();
 	indent_level.push_back(IndentLevel(0, 0));
-	_parse_class_contents(main_class);
+	_parse_class_member_initalizers(main_class);
 	if (error_set) {
 		return ERR_PARSE_ERROR;
 	}
@@ -8652,6 +8666,18 @@ Error GDScriptParser::_parse(const String &p_base_path) {
 	// Resolve all class-level stuff before getting into function blocks
 	_check_class_level_types(main_class);
 
+	if (error_set) {
+		return ERR_PARSE_ERROR;
+	}
+
+	if (interface_only) {
+		return OK;
+	}
+
+	// Parse the content of function blocks
+	indent_level.clear();
+	indent_level.push_back(IndentLevel(0, 0));
+	_parse_class_function_bodies(main_class);
 	if (error_set) {
 		return ERR_PARSE_ERROR;
 	}
@@ -8733,6 +8759,11 @@ Error GDScriptParser::parse(const String &p_code, const String &p_base_path, boo
 	return ret;
 }
 
+Error GDScriptParser::parse_interface(const String &p_code, const String &p_base_path, const String &p_self_path) {
+	interface_only = true;
+	return parse(p_code, p_base_path, false, p_self_path);
+}
+
 bool GDScriptParser::is_tool_script() const {
 
 	return (head && head->type == Node::TYPE_CLASS && static_cast<const ClassNode *>(head)->tool);
@@ -8781,6 +8812,7 @@ void GDScriptParser::clear() {
 	check_types = true;
 	dependencies_only = false;
 	dependencies.clear();
+	interface_only = false;
 	error = "";
 #ifdef DEBUG_ENABLED
 	safe_lines = NULL;
