6b0a162da56253832ddf434fd558bae754da27ec
George Marques <george@gmarqu.es>
2020-02-24 11:19:39 -0300

    Add flag to disable stdout/stderr output without disabling handlers
    This is useful to be able to capture the output without having it
    printed to the console.

--- core/print_string.h
+++ core/print_string.h
@@ -56,6 +56,8 @@ void remove_print_handler(PrintHandlerList *p_handler);
 
 extern bool _print_line_enabled;
 extern bool _print_error_enabled;
+extern bool _stdout_enabled;
+extern bool _stderr_enabled;
 extern void print_line(String p_string);
 extern void print_error(String p_string);
 extern void print_verbose(String p_string);
